f2aa61f065f32527e83d807e58ddbd34b0979e54
Olexandra Herasymenko <g.alecksandra@gmail.com>
2021-09-23 15:50:20 +0300

    add test

---
+++ src/test/java/com/my/PathTest.java
@@ -0,0 +1,13 @@
+package com.my;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PathTest {
+    @Test
+    public void test() {
+        new Path();
+    }
+
+}
---
+++ src/test/java/com/my/data_base/FieldsTest.java
@@ -0,0 +1,14 @@
+package com.my.data_base;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FieldsTest {
+
+
+    @Test
+    public void test() {
+        new Fields();
+    }
+}
